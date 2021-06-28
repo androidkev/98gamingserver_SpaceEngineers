e4e791eb0d7c3c6c28e28d0730bb407ce00c1830
androidkev <johnkevyncastro@gmail.com>
2021-06-28 22:27:34 +0800

    entrypoint space

--- entrypoint-space_engineers.bash
+++ entrypoint-space_engineers.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ~/.profile
-cd /appdata/space-engineers/SpaceEngineersDedicated/DedicatedServer64/
-env WINEARCH=win64 WINEDEBUG=-all WINEPREFIX=/wineprefix wine /appdata/space-engineers/SpaceEngineersDedicated/DedicatedServer64/SpaceEngineersDedicated.exe -noconsole -path Z:\\appdata\\space-engineers\\SpaceEngineersDedicated -ignorelastsession
+cd /appdata/space-engineers/config/DedicatedServer64/
+env WINEARCH=win64 WINEDEBUG=-all WINEPREFIX=/wineprefix wine /appdata/space-engineers/config/DedicatedServer64/SpaceEngineersDedicated.exe -noconsole -path Z:\\appdata\\space-engineers\\SpaceEngineersDedicated -ignorelastsession
