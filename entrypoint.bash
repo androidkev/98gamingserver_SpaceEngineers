30a0012db782f06a170085c96eac3ebd7b708b70
androidkev <johnkevyncastro@gmail.com>
2021-06-28 21:52:05 +0800

    update dir

--- entrypoint.bash
+++ entrypoint.bash
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 # #check if /appdata/space-engineers/config/World is a folder
-if [ ! -d "/appdata/space-engineers/World" ]; then
+if [ ! -d "/appdata/space-engineers/config/World" ]; then
   echo "World folder does not exist, exiting"
   exit 129
 fi
 
 # #check if /appdata/space-engineers/config/World/Sandbox.sbc exists and is a file
-if [ ! -f "/appdata/space-engineers/World/Sandbox.sbc" ]; then
+if [ ! -f "/appdata/space-engineers/config/World/Sandbox.sbc" ]; then
   echo "Sandbox.sbc file does not exist, exiting."
   exit 130
 fi
 
 # #check if /appdata/space-engineers/config/SpaceEngineers-Dedicated.cfg is a file
-if [ ! -f "/appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg" ]; then
+if [ ! -f "/appdata/space-engineers/config/SpaceEngineers-Dedicated.cfg" ]; then
   echo "SpaceEngineers-Dedicated.cfg file does not exist, exiting."
   exit 131
 fi
 
 #set <LoadWorld> to the correct value
-cat /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg | sed -E '/.*LoadWorld.*/c\  <LoadWorld>Z:\\appdata\\space-engineers\\World</LoadWorld>' > /tmp/SpaceEngineers-Dedicated.cfg && cat /tmp/SpaceEngineers-Dedicated.cfg > /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg
+cat /appdata/space-engineers/config/SpaceEngineers-Dedicated.cfg | sed -E '/.*LoadWorld.*/c\  <LoadWorld>Z:\\appdata\\space-engineers\\World</LoadWorld>' > /tmp/SpaceEngineers-Dedicated.cfg && cat /tmp/SpaceEngineers-Dedicated.cfg > /appdata/space-engineers/config/SpaceEngineers-Dedicated.cfg
 
 #set game port to the correct value
-#cat /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg | sed -E '/.*ServerPort.*/c\  <ServerPort>27016</ServerPort>' > /tmp/SpaceEngineers-Dedicated.cfg && cat /tmp/SpaceEngineers-Dedicated.cfg > /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg
+#cat /appdata/space-engineers/config/SpaceEngineers-Dedicated.cfg | sed -E '/.*ServerPort.*/c\  <ServerPort>27016</ServerPort>' > /tmp/SpaceEngineers-Dedicated.cfg && cat /tmp/SpaceEngineers-Dedicated.cfg > /appdata/space-engineers/config/SpaceEngineers-Dedicated.cfg
 
 #configure plugins section in SpaceEngineers-Dedicated.cfg
 #get new plugins string
@@ -40,11 +40,11 @@ SED_EXPRESSION_EMPTY="s/<Plugins \/>/${PLUGINS_STRING////\\/} /g"
 SED_EXPRESSION_FULL="s/<Plugins>.*<\/Plugins>/${PLUGINS_STRING////\\/} /g"
 
 #find and replace in SpaceEngineers-Dedicated.cfg for empty "<Plugins />" element
-cat /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg | sed -E "$SED_EXPRESSION_EMPTY" > /tmp/SpaceEngineers-Dedicated.cfg && cat /tmp/SpaceEngineers-Dedicated.cfg > /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg
+cat /appdata/space-engineers/config/SpaceEngineers-Dedicated.cfg | sed -E "$SED_EXPRESSION_EMPTY" > /tmp/SpaceEngineers-Dedicated.cfg && cat /tmp/SpaceEngineers-Dedicated.cfg > /appdata/space-engineers/config/SpaceEngineers-Dedicated.cfg
 
 #find and replace in SpaceEngineers-Dedicated.cfg for filled out "<Plugins>...</Plugins>" element
 # sed can't handle multiple lines easily, so everything needs to be on a single line.
-cat /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg | sed -E "$SED_EXPRESSION_FULL" > /tmp/SpaceEngineers-Dedicated.cfg && cat /tmp/SpaceEngineers-Dedicated.cfg > /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg
+cat /appdata/space-engineers/config/SpaceEngineers-Dedicated.cfg | sed -E "$SED_EXPRESSION_FULL" > /tmp/SpaceEngineers-Dedicated.cfg && cat /tmp/SpaceEngineers-Dedicated.cfg > /appdata/space-engineers/config/SpaceEngineers-Dedicated.cfg
 
 #check if the owner of the appdata folder exists, and if they don't, create them.
 TARGET_UID=$(ls -nd /appdata/space-engineers | awk '{ print $3 }')
@@ -60,5 +60,5 @@ echo "Starting Space Engineers Dedicated Server with user: $TARGET_USER uid $TAR
 chown $TARGET_USER /wineprefix
 chown -R $TARGET_USER:$TARGET_GID /appdata/space-engineers/World
 
-runuser -l $TARGET_USER bash -c '/usr/games/steamcmd +login anonymous +@sSteamCmdForcePlatformType windows +force_install_dir /appdata/space-engineers/SpaceEngineersDedicated +app_update 298740 +quit'
+runuser -l $TARGET_USER bash -c '/usr/games/steamcmd +login anonymous +@sSteamCmdForcePlatformType windows +force_install_dir /appdata/space-engineers/config +app_update 298740 +quit'
 runuser -l $TARGET_USER bash -c '/entrypoint-space_engineers.bash'
